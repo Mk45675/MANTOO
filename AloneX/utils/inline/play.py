93f2b05a2330ee32c78d6383d322c1f2af575055
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2025-04-03 23:19:03 +0530

    Update play.py

--- AloneX/utils/inline/play.py
+++ AloneX/utils/inline/play.py
@@ -54,7 +54,7 @@ def stream_markup_timer(_, chat_id, played, dur):
     else:
         bar = "╌╌╌╌╌╌╌╌╌♡"
     buttons = [
-        
+        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
